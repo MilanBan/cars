befe5160728a445d1d8a25ed72fe0186fcad6f48
Milan Bancevic <m.bancevic@gmail.com>
2020-02-17 10:18:52 +0100

    Zadatak 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to Cars Project
+                    Welcome {{ $name }}
                 </div>
 
                 <div class="links">
